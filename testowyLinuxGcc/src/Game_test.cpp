487238c4677e8818ea273db7b73ab6a071962459
janek <jj>
2018-11-14 23:22:10 +0100

    Adding files for CLion. Cmake is the most important one.

--- testowyLinuxGcc/src/Game_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Game_test.cpp
- *
- *  Created on: 18 wrz 2017
- *      Author: janek
- */
-
-#include "Game.hpp"
-
--- testowyLinuxGcc/src/Position_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Position_test.cpp
- *
- *  Created on: 18 wrz 2017
- *      Author: janek
- */
-
-#include "Position.hpp"
-
